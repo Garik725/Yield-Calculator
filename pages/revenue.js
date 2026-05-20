15a5a72787919527a14331519ab9c075b20c9bd9
Garik725 <garik.minasyan08@gmail.com>
2026-05-20 16:13:23 +0400

    Update revenue.js

--- pages/revenue.js
+++ pages/revenue.js
@@ -145,6 +145,7 @@ export default function Revenue() {
             <Link href="/portfolio" className="hd-link">Portfolio</Link>
             <Link href="/curve" className="hd-link">Yield Curve</Link>
             <Link href="/fx" className="hd-link">FX</Link>
+            <Link href="/risk" className="hd-link">Risk</Link>
           </nav>
         </div>
       </header>
